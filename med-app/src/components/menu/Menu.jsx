5b582b4c6a031c941528e680517186db3f71738f
Lukasz Chwiedosik <lukasz.chwiedosik@gmail.com>
2023-04-09 00:23:59 +0200

    Feat: stylized nav bar

--- med-app/src/components/menu/Menu.jsx
+++ med-app/src/components/menu/Menu.jsx
@@ -1,28 +1,42 @@
-import React, { useState } from 'react';
-import { AppBar, Toolbar, Drawer, Box, IconButton, Typography } from '@mui/material';
+import React, { useContext, useState } from 'react';
+import { AppBar, Toolbar, Drawer, Box, IconButton, Typography, Button } from '@mui/material';
 import { Sx } from './menu.style';
 import MenuIcon from '@mui/icons-material/Menu';
+import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
+import { LoginContext } from '../../contexts/loginContext';
 
 const Menu = () => {
 
+    const { loggedState, setLoggedState } = useContext(LoginContext)
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+    const handleLogOutUser = () => setLoggedState(!loggedState);
 
     return (
         <>
             <AppBar position="static">
                 <Toolbar variant="dense" sx={Sx.toolbarSx}>
-                    <IconButton onClick={() => setIsDrawerOpen(true)}>
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant='h6'>
-                        Logo
-                    </Typography>
+                    <Box sx={Sx.innerBoxOne} component='div'>
+                        {loggedState &&
+                            <IconButton onClick={() => setIsDrawerOpen(true)}>
+                                <MenuIcon />
+                            </IconButton>
+                        }
+                        <Typography variant='h6' sx={{ padding: '8px' }}>
+                            Logo
+                        </Typography>
+                    </Box>
+                    <Box sx={Sx.innerBoxTwo}>
+                        {loggedState &&
+                            <Button onClick={handleLogOutUser} type='button' variant="contained" startIcon={<PowerSettingsNewIcon />} >Wyloguj</Button>
+                        }
+                    </Box>
                     <Drawer
                         anchor='left'
                         open={isDrawerOpen}
                         onClose={() => setIsDrawerOpen(false)}
                     >
-                        <Box sx={Sx.boxSx}>
+                        <Box sx={Sx.drawerBox}>
 
                         </Box>
                     </Drawer>
--- med-app/src/components/menu/menu.style.js
+++ med-app/src/components/menu/menu.style.js
@@ -1,10 +1,22 @@
 export const Sx = {
     toolbarSx : {
         height: '50px',
+        display: 'flex',
+        justifyContent: 'space-bettwen'
     },
-    boxSx: {
+    drawerBox: {
         padding: '2px',
         width: '320px',
         textAlign: 'center'
+    },
+    innerBoxOne: {
+        width: '50%',
+        display: 'flex',
+        aligItems: 'center',
+        justifyContent: 'flex-start'
+    },
+    innerBoxTwo: {
+        width: '50%',
+        textAlign: 'right'
     }
 }
