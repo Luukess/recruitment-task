63271c342efde01f1968811cff9ef8ca02253047
Lukasz Chwiedosik <lukasz.chwiedosik@gmail.com>
2023-04-09 12:06:05 +0200

    Feat: removed example style for item in grid in home page component

--- med-app/src/pages/home-page/HomePage.jsx
+++ med-app/src/pages/home-page/HomePage.jsx
@@ -7,13 +7,6 @@ import { styled } from '@mui/material/styles';
 
 const HomePage = () => {
 
-    const Item = styled(Paper)(({ theme }) => ({
-        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-        ...theme.typography.body2,
-        padding: theme.spacing(1),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    }));
 
     return (
         <Container fixed sx={{ backgroundColor: 'red', height: '100%'}}>
