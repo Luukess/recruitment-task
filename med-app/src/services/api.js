c42ac75535c5ca107c919092a083a579f665e39d
Lukasz Chwiedosik <lukasz.chwiedosik@gmail.com>
2023-04-10 14:04:10 +0200

    Feat: added service with json server api

---
+++ med-app/src/services/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const instanceConnectApi = axios.create({
+    timeout: 6000,
+    baseURL: 'http://localhost:3000',
+    headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+});
+
+export const handleGetPatients = async () => {
+    return instanceConnectApi.get('/patients');
+};
